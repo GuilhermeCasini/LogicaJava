4cc208d8d3198a59deec0a18fb695def5c1dddd0
GuilhermeCasini <91803425+GuilhermeCasini@users.noreply.github.com>
2022-06-28 13:34:13 -0300

    Add files via upload

--- poo/src/exercicios/Exercicio1.java
+++ poo/src/exercicios/Exercicio1.java
@@ -4,4 +4,24 @@ public class Exercicio1 {
 	private double capacidade;
 	private int passageiros;
 	private double consumo;
+	
+	public double getCapacidade() {
+		return capacidade;
+	}
+	public void setCapacidade(double capacidade) {
+		this.capacidade = capacidade;
+	}
+	public int getPassageiros() {
+		return passageiros;
+	}
+	public void setPassageiros(int passageiros) {
+		this.passageiros = passageiros;
+	}
+	public double getConsumo() {
+		return consumo;
+	}
+	public void setConsumo(double consumo) {
+		this.consumo = consumo;
+	}
+	
 }
